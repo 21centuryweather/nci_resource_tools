a11a16fa41b9c6e34bbde7b160bfd64c634ef735
Paul Gregory <pag548@gadi-login-07.gadi.nci.org.au>
2025-05-05 17:21:05 +1000

    UPDATES : Fixed path in env.sh

--- env.sh
+++ env.sh
@@ -6,12 +6,19 @@
 module use /g/data/xp65/public/modules
 module load conda/analysis3
 
+# Load a virtual python environment built on xp65 - this includes 
+# 'pymunge' library used by Scott Wales' nci tools repo
+source /g/data/gb02/public/hackathon_env/bin/activate
+
 # Root directory for this repo
-export ROOT=/home/548/${USER}/code/nci_resource_tools
+export ROOT=/g/data/gb02/public/code/nci_resource_tools
 export MODULES=${ROOT}/nci_resource_tools
 
-# Location of Scott Wale's tools
-export TOOLS=/home/548/${USER}/code/nci-tools/src/ncitools
+# Location of Scott Wales' tools
+export TOOLS=/g/data/gb02/public/code/nci-tools/src/ncitools
 
 # Append to our python path
 export PYTHONPATH=${MODULES}:${TOOLS}:${PYTHONPATH}
+
+# Add quarto to our path
+export PATH=${PATH}:/g/data/gb02/pag548/quarto-1.7.29/bin/
