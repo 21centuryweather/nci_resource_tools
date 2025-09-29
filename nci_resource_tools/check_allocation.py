da238616a2f19c6f1ab1849bc9347d876cfcf743
Pao Corrales <paobcorrales@gmail.com>
2025-09-29 16:37:08 +1000

    Add table with user info

--- nci_resource_tools/check_allocation.py
+++ nci_resource_tools/check_allocation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # Create a dictionary of allocation vs usage dataframes
 
@@ -34,3 +35,33 @@ def check_allocation(grant_dict,
         alloc_df_dict[project] = alloc_check_df
 
     return alloc_df_dict 
+
+def create_user_table(SU_df_dict1, 
+                      allocation_df,
+                      ):
+
+    """
+    Create a table of usage by user for a given project
+    """   
+    user_totals = {}
+    for i,project in enumerate(SU_df_dict1.keys()):
+        
+        user_totals[project] = SU_df_dict1[project].groupby('user')['usage'].sum()
+
+    table_by_user = pd.DataFrame(pd.concat(user_totals)).reset_index(names=['project', 'user'])
+
+    table_by_user['usage'] = table_by_user['usage'].round(1)
+    table_by_user = table_by_user.merge(allocation_df, on=['project','user'], how='left').fillna(5)
+    table_by_user['allocation'] = table_by_user['allocation'].round(1)
+    table_by_user['status'] = np.where(table_by_user['usage'] > table_by_user['allocation'],
+    'Usage exceeds allocation by ' + (table_by_user['usage'] - table_by_user['allocation']).round(1).astype(str),
+    'Within 5 KSU allocation per quarter')
+
+    # # Format table
+    table_by_user.columns = [col.capitalize() for col in table_by_user.columns]
+
+    def highlight_over_allocation(row):
+        warning = 'exceeds' in row['Status']
+        return ['color: #B7533D' if warning else '' for _ in row]
+
+    return table_by_user.style.apply(highlight_over_allocation, axis=1).format(precision=1).hide(axis=0)
